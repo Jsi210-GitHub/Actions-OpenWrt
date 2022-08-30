cfbbb5a1d995170788a4bbddcdf8b75ee0fab4f3
Jsi210-GitHub <61343866+Jsi210-GitHub@users.noreply.github.com>
2022-08-30 08:56:12 +0800

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -14,5 +14,5 @@
 #sed -i 's/^#\(.*helloworld\)/\1/' feeds.conf.default
 
 # Add a feed source
-echo 'src-git kenzo https://github.com/kenzok8/openwrt-packages' >>feeds.conf.default
-echo 'src-git small https://github.com/kenzok8/small' >>feeds.conf.default
+echo 'src-git small8 https://github.com/kenzok8/small-package' >>feeds.conf.default
+echo 'src/gz openwrt_kiddin9 https://op.supes.top/packages/x86_64' >>feeds.conf.default
